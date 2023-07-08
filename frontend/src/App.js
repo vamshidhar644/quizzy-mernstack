96e9739819b33569e37d894ae50d1e17b4348c14
VAMSHIDHAR DAWOOR <vamshidhar.20bce7285@vitap.ac.in>
2023-07-08 18:26:00 +0530

    Initial files

---
+++ frontend/src/App.js
@@ -0,0 +1,48 @@
+import './styles/App.css';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+/** import components */
+import Main from './components/Main';
+import Quiz from './components/Quiz';
+import Result from './components/Result';
+import { CheckUserExist } from './helper/helper';
+import Dashboard from './pages/Dashboard';
+
+/** react routes */
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />,
+  },
+  {
+    path: '/quiz-home',
+    element: <Main></Main>,
+  },
+  {
+    path: '/quiz',
+    element: (
+      <CheckUserExist>
+        <Quiz />
+      </CheckUserExist>
+    ),
+  },
+  {
+    path: '/result',
+    element: (
+      <CheckUserExist>
+        <Result />
+      </CheckUserExist>
+    ),
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
